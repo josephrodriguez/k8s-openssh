308c88dbe5c008439231048653ca1b12e5d9c22a
José Miguel Rodríguez García <josemrodriguez89@gmail.com>
2022-08-12 00:56:28 +0300

    Update ssh private and public keys permissions

--- scripts/k8s-openssh.sh
+++ scripts/k8s-openssh.sh
@@ -9,60 +9,14 @@
 # Returns:
 #   None
 # ==============================================================================
-function get_ssh_public_key() {
-
-    SSH_DIR=$(dirname $SSH_KEY_PATH)
-
-    if [ ! -d "$SSH_DIR" ]; then
-        warn "$SSH_DIR not found"
-        debug "Create and set permissions to SSH key directory: $SSH_DIR"
-        mkdir -p $SSH_DIR && chmod 700 $SSH_DIR
-    fi
-
-    SSH_PUBLIC_KEY_PATH="${SSH_KEY_PATH%.*}.pub"
-
-    if [ ! -f "$SSH_KEY_PATH" ]; then 
-        warn "SSH key file $SSH_KEY_PATH not found"
-
-        debug "Generating SSH key: $SSH_KEY_PATH"
-        debug "SSH key type: $SSH_KEY_TYPE"
-        debug "SSH key size: $SSH_KEY_SIZE"
-        debug "SSH username: $SSH_USERNAME"
-        debug "SSH server: $SSH_SERVER_NAME"
-
-        generate_ssh_key $SSH_KEY_TYPE $SSH_KEY_SIZE $SSH_KEY_PATH $SSH_USERNAME $SSH_SERVER_NAME
-
-        debug "Extracting public key for SSH key: $SSH_KEY_PATH"        
-        
-        touch $SSH_PUBLIC_KEY_PATH && chmod 700 $SSH_PUBLIC_KEY_PATH
-        $(extract_public_key $SSH_KEY_PATH) > $SSH_PUBLIC_KEY_PATH
-    fi
-
-    if [ ! -f "$SSH_PUBLIC_KEY_PATH" ]; then
-        error "$SSH public key $SSH_PUBLIC_KEY_PATH not found"
-        exit 1
-    fi
-
-    cat "$SSH_PUBLIC_KEY_PATH"
-}
-
-# ==============================================================================
-# Method description
-# Arguments:
-#   Key type
-#   Number of bits for the key
-#   Filename: The output path for the SSH key
-# Returns:
-#   None
-# ==============================================================================
 function deploy_k8s_openssh_server() {
 
     info "Creating K8S deployment resources..."
 
-    SSH_PUBLIC_KEY=$(get_ssh_public_key)
+    public_key=$(get_ssh_public_key "$SSH_KEY_PATH" "$SSH_KEY_TYPE" "$SSH_KEY_SIZE" "$SSH_USERNAME" "$SSH_SERVER_NAME")
 
     debug $(k8s_create_namespace_if_not_exist "$K8S_NAMESPACE_NAME" "$K8S_DRY_RUN")
-    debug $(k8s_create_secret "$K8S_SECRET_ENV_NAME" "$K8S_NAMESPACE_NAME" "$SSH_PUBLIC_KEY" "$K8S_DRY_RUN")
+    debug $(k8s_create_secret "$K8S_SECRET_ENV_NAME" "$K8S_NAMESPACE_NAME" "$public_key" "$K8S_DRY_RUN")
 
     ENV_PROPERTIES_FILE_PATH=$(create_properties_file $PGID $PUID $SUDO_ACCESS $TZ)
 
